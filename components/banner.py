b0db519571a5d8f49f42598662cd62bc1febf5e4
Your Name <brianbell0505@gmail.com>
2025-02-27 14:04:54 -0600

    Banner Update

--- components/banner.py
+++ components/banner.py
@@ -3,5 +3,5 @@ import streamlit as st
 def show_alpha_banner():
     """Display an alpha version banner across the application."""
     st.info(
-        "Current Prize: $1,000 USDT - This is an alpha version. Expect bugs and rapid changes... even possibly the name."
+        "This is an alpha version. Expect bugs and rapid changes... even possibly the name."
     ) 
